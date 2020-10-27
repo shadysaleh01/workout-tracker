63e2c8ca223a184872a54f7b0409bbe1be2d31fc
Shady Coder <shadysaleh01@gmail.com>
2020-10-27 15:44:30 -0700

    Created api POST router to create new workout in the database

--- develop/routes/api-routes.js
+++ develop/routes/api-routes.js
@@ -12,5 +12,14 @@ router.get("/api/workouts", (req, res) => {
       });
 });
 
+router.post("/api/workouts", ({ body }, res) => {
+   Workout.create(body)
+      .then((dbWorkouts) => {
+         res.json(dbWorkouts);
+      })
+      .catch((err) => {
+         res.status(400).json(err);
+      });
+});
 
 module.exports = router;
