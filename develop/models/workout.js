611d111c001f122a003367ed52ea59f47d1ff41f
Shady Coder <shadysaleh01@gmail.com>
2020-10-27 01:21:58 -0700

    Created all colums needed in the schema

--- develop/models/workout.js
+++ develop/models/workout.js
@@ -3,6 +3,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+   name: {
+      type: String,
+      trim: true,
+      required: "Enter an exercise type"
+   },
+   type: {
+      type: String,
+      trim: true,
+      required: "Enter an exercise name"
+   },
+   weight: {
+      type: Number
+   },
+   sets: {
+      type: Number
+   },
+   reps: {
+      type: Number
+   },
+   duration: {
+      type: Number,
+      trim: true,
+      required: "Enter an exercise duration in minutes"
+   },
+   distance: {
+      type: Number
+   }
 
-
-})
+});
