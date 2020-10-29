b47c9ff67e6e16124c01ba267780253088ac7df3
Shady Coder <shadysaleh01@gmail.com>
2020-10-29 01:15:40 -0700

    Created index.js in models folder and exports workout.js

--- develop/routes/api-routes.js
+++ develop/routes/api-routes.js
@@ -22,4 +22,15 @@ router.post("/api/workouts", ({ body }, res) => {
       });
 });
 
+router.put("/api/workouts/:id", (req, res) => {
+
+   Workout.update(req.parms.id)
+      .then((dbWorkouts) => {
+         res.json(dbWorkouts);
+      })
+      .catch((err) => {
+         res.status(404).json(err);
+      });
+});
+
 module.exports = router;
