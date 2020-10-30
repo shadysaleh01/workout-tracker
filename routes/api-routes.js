6bec4dfe0afc027052745ac16a1d4dc727d326f0
Shady Coder <shadysaleh01@gmail.com>
2020-10-30 02:37:51 -0700

    PUT api to add exercise to workout obj

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,6 +12,31 @@ router.get("/workouts", (req, res) => {
       });
 });
 
+// addExercise PUT /api/workouts/ 
+
+router.put("/workouts/:id", ({ params, body }, res) => {
+   console.log(body);
+   db.Workout.findByIdAndUpdate(
+      params.id,
+      {
+         $push: {
+            exercises: body,
+
+         },
+      },
+      {
+         new: true,
+         runValidators: true
+      }
+   )
+      .then(dbWorkout => {
+         res.json(dbWorkout);
+      })
+      .catch(err => {
+         res.json(err);
+      });
+});
+
 // createWorkout POST /api/workouts
 router.post("/workouts", ({ body }, res) => {
    console.log(body);
