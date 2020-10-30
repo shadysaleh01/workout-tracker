7bc1314deea2203714a37964ed2a22eb48334360
Shady Coder <shadysaleh01@gmail.com>
2020-10-30 00:33:16 -0700

    reorganized all my files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const db = require("../models");
 
+// GET api to get workout 
 router.get("/api/workouts", (req, res) => {
    db.Workout.find({})
       .then((dbWorkouts) => {
@@ -10,8 +11,9 @@ router.get("/api/workouts", (req, res) => {
          res.status(400).json(err);
       });
 });
-
+// POST api to create new workout
 router.post("/api/workouts", ({ body }, res) => {
+
    db.Workout.create(body)
       .then((dbWorkouts) => {
          res.json(dbWorkouts);
@@ -21,9 +23,20 @@ router.post("/api/workouts", ({ body }, res) => {
       });
 });
 
-router.put("/api/workouts/:id", (req, res) => {
+// router.put("/api/workouts/:id", (req, res) => {
 
-   db.Workout.update(req.parms.id)
+//    db.Workout.update(req.parms.id)
+//       .then((dbWorkouts) => {
+//          res.json(dbWorkouts);
+//       })
+//       .catch((err) => {
+//          res.status(404).json(err);
+//       });
+// });
+
+router.put("/api/workouts/:id", (req, res) => {
+   console.log(req)
+   db.Workout.findByIdAndUpdate(req.params.id, updateObj, { new: true })
       .then((dbWorkouts) => {
          res.json(dbWorkouts);
       })
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Workout-Tracker
-
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -7,7 +6,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 
-const db = require("./models/workout");
+const db = require("./models");
 
 
 const app = express();
@@ -25,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://jamierachael:Cheeseme1@ds
 
 // Requiring our routes
 
-// app.use("/api", require("./routes/api-routes.js"));
+app.use("/api", require("./routes/api-routes.js"));
 app.use("/", require("./routes/html-routes.js"));
 
 
