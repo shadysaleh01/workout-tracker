aa4985eddc12c9417640c705526038d7b2659f67
Shady Coder <shadysaleh01@gmail.com>
2020-10-30 02:35:54 -0700

    virtual to add a dynamically-created property to schema

--- models/workout.js
+++ models/workout.js
@@ -44,7 +44,13 @@ const workoutSchema = new Schema({
       }
    });
 
-
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+   // "reduce" array of exercises down to just the sum of their durations
+   return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+   }, 0);
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
